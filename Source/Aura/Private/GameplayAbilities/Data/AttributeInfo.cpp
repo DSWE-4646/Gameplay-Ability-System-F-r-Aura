637be7f90b69656741983fa724cef374d7a3f5db
SiberianSledCat <dy464697573@gmail.com>
2026-02-06 17:47:13 +0800

    GameplayTag数据资产

---
+++ Source/Aura/Private/GameplayAbilities/Data/AttributeInfo.cpp
@@ -0,0 +1,24 @@
+﻿// SiberianSledCat
+
+
+#include "GameplayAbilities/Data/AttributeInfo.h"
+
+FAuraAttributeInfo UAttributeInfo::GetAttributeInfo(const FGameplayTag& AttributeTag, bool& bLogNotFound) const
+{
+	for (const FAuraAttributeInfo& info : AttributeInformation)
+	{
+		if (info.AttributeTag.MatchesTagExact(AttributeTag))
+		{
+			return info;
+		}
+	}
+
+	
+	if (bLogNotFound)
+	{
+		UE_LOG(LogTemp, Error, TEXT("无法找到AttributeTag: %s 在 AttributeInfo: %s 中"),
+			*AttributeTag.ToString(),
+			*GetNameSafe(this)); //GetNameSafe() 防止this为空时崩溃,相比于GetName() 会先判断指针是否存在
+	}
+	return FAuraAttributeInfo();
+}
---
+++ Source/Aura/Public/GameplayAbilities/Data/AttributeInfo.h
@@ -0,0 +1,50 @@
+﻿// SiberianSledCat
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
+#include "Engine/DataAsset.h"
+#include "AttributeInfo.generated.h"
+
+/**
+ * 
+ */
+
+USTRUCT(BlueprintType)
+struct FAuraAttributeInfo
+{
+	GENERATED_BODY()
+
+	/* 用于标识属性的GameplayTag */
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
+	FGameplayTag AttributeTag;
+
+	/* 用户界面文本显示的名称 */
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
+	FText DisplayName;
+
+	/* 属性描述文本 */
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
+	FText Description;
+
+	/* 属性当前数值（仅蓝图可读，不暴露给数据资产编辑） */
+	UPROPERTY(BlueprintReadOnly)
+	float AttributeValue = 0.f;
+};
+
+UCLASS()
+class AURA_API UAttributeInfo : public UDataAsset
+{
+	GENERATED_BODY()
+public:
+	/* 在属性信息数组中查找指定Tag对应的信息，如果找到则返回对应的信息，否则返回空信息*/
+	UFUNCTION(BlueprintCallable)
+	FAuraAttributeInfo GetAttributeInfo(const FGameplayTag& AttributeTag, bool& bFound) const;
+	
+protected:
+	/* 属性信息数组 */
+	UPROPERTY(BlueprintReadOnly, EditDefaultsOnly)
+	TArray<FAuraAttributeInfo> AttributeInformation;
+	
+};
